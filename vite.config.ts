1c34918ecf600ba29f3473d2feeac44980af23f3
gkmgggggg <2319454362@qq.com>
2021-04-20 11:03:41 +0800

    修复引用错误

--- vite.config.ts
+++ vite.config.ts
@@ -8,16 +8,16 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
-      '_c': resolve(__dirname, 'src/components')
+      _c: resolve(__dirname, 'src/components')
     }
   },
-  base: './', //设置打包路径
+  base: './', // 设置打包路径
   server: {
-    port: 8080,//端口号
-    open: true,//启动服务时是否自动打开游览器
-    cors: true //允许跨域
+    port: 8080, // 端口号
+    open: true, // 启动服务时是否自动打开游览器
+    cors: true // 允许跨域
 
-    //设置代理
+    // 设置代理
     // proxy: {
     //   '/api': {
     //     target: 'http://xxx.xxx.xxx.xxx:8000',
